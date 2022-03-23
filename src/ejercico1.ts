1d47db7626309d83fff5785d6a06220c93f88499
EugeniaLosinno <102192294+EugeniaLosinno@users.noreply.github.com>
2022-03-23 00:12:04 -0300

    Initial commit

---
+++ src/ejercico1.ts
@@ -0,0 +1,18 @@
+console.log(" 1.- Prender el horno");
+console.log(" 2.- Tomar un molde");
+console.log(" 3.- Enmantecar el molde");
+console.log(" 4.- Poner harina al molde");
+console.log(" 5.- Tomar un bols");
+console.log(" 6.- Poner la manteca a temoperatuta ambiente en el bols");
+console.log(" 7.- Agregar el azucar al bols");
+console.log(
+  " 8.- Batir la manteca con el azucar hasta que la preparación este cremosa"
+);
+console.log(" 9.- Agreagr 2 huevos de a uno, los huevos atemperatura ambiente");
+console.log("10.- Agregar la harina intercalada con la leche");
+console.log("11.- Poner la preparación en el molde");
+console.log("12.- Levarla al horno");
+console.log("13.- Dejar la torta 40 minutos en horno sin abrir");
+console.log("14.- Ponerle un cuchillo y ver si esta cocida");
+console.log("15.- Salarca del horno");
+console.log("16.- desmoldarla y ponderla en un plato");
